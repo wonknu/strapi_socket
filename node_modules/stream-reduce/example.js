4d4764026ee2b92e907873ebf4c774c9d823123d
wonknu10 <fabrice.labbe@adfab.fr>
2018-02-16 16:58:54 -0500

    rm node modlue

--- node_modules/stream-reduce/example.js
+++
@@ -1,6 +0,0 @@
-var reduce = require("./");
-process.stdin.pipe(reduce(function(acc, data) {
-  return acc + data.length;
-}, 0)).on("data", function(length) {
-  console.log("stdin size:", length);
-});
